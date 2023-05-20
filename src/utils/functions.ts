5121eb31bb1f642191449f7948ee9f493bf28c96
Shamanth <shamanth1604@gmail.com>
2023-05-20 18:56:00 +0530

    Added utility function to get participant image

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -7,9 +7,25 @@ export const formatUsernames = (
     let usernames: Array<string> = [];
     if (participants !== null) {
         usernames = participants
-            .filter((participant) => participant!.user?.id != myUserId)
+            .filter((participant) => participant!.user?.id !== myUserId)
             .map((participant) => participant!.user?.username as string);
     }
 
     return usernames.join(", ");
+};
+
+export const participantImage = (
+    participants: Maybe<Maybe<Participant>[]>,
+    myUserId: string
+): string => {
+    if (participants != null) {
+        if (participants.length == 2) {
+            return (
+                participants.filter(
+                    (participant) => participant?.user?.id !== myUserId
+                )[0]?.user?.image ?? ""
+            );
+        }
+    }
+    return "";
 };
