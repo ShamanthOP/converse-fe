5ddca97da1087b5068b99887d42a5227f2bd9647
Shamanth <shamanth1604@gmail.com>
2023-05-20 18:56:30 +0530

    Conversation id is removed from URL on logout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,23 @@ import { authOptions } from "./api/auth/[...nextauth]";
 import { Box } from "@chakra-ui/react";
 import Chat from "@/components/Chat";
 import Auth from "@/components/Auth";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
     const { data: session } = useSession();
+    const router = useRouter();
 
     const reloadSession = () => {
         const event = new Event("visibilitychange");
         document.dispatchEvent(event);
     };
+
+    useEffect(() => {
+        if (!session?.user && router.query.conversationId) {
+            router.replace(process.env.NEXT_PUBLIC_BASE_URL as string);
+        }
+    }, [session?.user, router.query, router]);
 
     return (
         <Box>
